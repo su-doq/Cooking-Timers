646d3f57ac141b7f8376deb63f618a21a551552d
Tim <husstim@gmail.com>
2019-05-15 14:20:06 -0400

    Clickable heart :)

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,16 @@ class RandomWordState extends State<RandomWords> {
       trailing: Icon(
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null
-      )
+      ),
+      onTap: () {
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
